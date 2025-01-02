2557fd5f5f8ef599b4d890adf3d3bb631cf6da5c
Almaz Saidov <almaz.saidov.03@bk.ru>
2025-01-02 18:54:33 +0300

    done with mappers

--- src/application/mappers.py
+++ src/application/mappers.py
@@ -1,68 +1,60 @@
-from datetime import date
-
-from sqlalchemy.dialects.postgresql import UUID
+import json
 
 from application.models import Address, Client, Image, Product, Supplier
 
 
 class AddressDTO:
-    def __init__(self, id: int, country: str, city: str, street: str):
-        self.id = id
-        self.country = country
-        self.city = city
-        self.street = street
+    def __init__(self, address: Address):
+        self.id = address.id
+        self.country = address.country
+        self.city = address.city
+        self.street = address.street
+
+    def map_address_to_dto(self):
+        return json.dumps(self.__dict__, indent=4)
 
 
 class ClientDTO:
-    def __init__(self, id: int, client_name: str, client_surname: str, birthday: date, gender: str, registration_date: date):
-        self.id = id
-        self.client_name = client_name
-        self.client_surname = client_surname
-        self.birthday = birthday
-        self.gender = gender
-        self.registration_date = registration_date
+    def __init__(self, client: Client):
+        self.id = client.id
+        self.client_name = client.client_name
+        self.client_surname = client.client_surname
+        self.birthday = client.birthday
+        self.gender = client.gender
+        self.registration_date = client.registration_date
+
+    def map_client_to_dto(self):
+        return json.dumps(self.__dict__, indent=4)
+
 
 
 class ImageDTO:
-    def __init__(self, id: UUID, image: bytes):
-        self.id = id
-        self.image = image
+    def __init__(self, image: Image):
+        self.id = image.id
+        self._image = image._image
+
+    def map_image_to_dto(self):
+        return json.dumps(self.__dict__, indent=4)
 
 
 class ProductDTO:
-    def __init__(self, id: int, name: str, category: str, price: int, available_stock: int, last_update_date: date):
-        self.id = id
-        self.name = name
-        self.category = category
-        self.price = price
-        self.available_stock = available_stock
-        self.last_update_date = last_update_date
+    def __init__(self, product: Product):
+        self.id = product.id
+        self.name = product.name
+        self.category = product.category
+        self.price = product.price
+        self.available_stock = product.available_stock
+        self.last_update_date = product.last_update_date
+
+    def map_product_to_dto(self):
+        return json.dumps(self.__dict__, indent=4)
 
 
 class SupplierDTO:
-    def __init__(self, id: int, name: str, phone_number: str):
-        self.id = id
-        self.name = name
-        self.phone_number = phone_number
+    def __init__(self, supplier: Supplier):
+        self.id = supplier.id
+        self.name = supplier.name
+        self.phone_number = supplier.phone_number
 
-
-def map_address_to_dto(address: Address) -> AddressDTO:
-    return AddressDTO(id=address.id, country=address.country, city=address.city, street=address.street)
-
-
-def map_client_to_dto(client: Client) -> ClientDTO:
-    return ClientDTO(id=client.id, client_name=client.client_name, client_surname=client.client_surname, birthday=client.birthday, gender=client.gender, registration_date=client.registration_date)
-
-
-def map_image_to_dto(image: Image) -> ImageDTO:
-    return ImageDTO(id=image.id, image=image._image)
-
-
-def map_product_to_dto(product: Product) -> ProductDTO:
-    return ProductDTO(id=product.id, name=product.name, price=product.price, available_stock=product.available_stock, last_update_date=product.last_update_date)
-
-
-def map_supplier_to_dto(supplier: Supplier) -> SupplierDTO:
-    return SupplierDTO(id=supplier.id, name=supplier.name, phone_number=supplier.phone_number)
-
-
+    def map_supplier_to_dto(self):
+        return json.dumps(self.__dict__, indent=4)
