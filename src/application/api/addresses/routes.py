eaad7b51214177107d99b02652ab7500e04679a3
Almaz Saidov <almaz.saidov.03@bk.ru>
2025-01-02 18:21:27 +0300

    done with creating and deleting addresses

--- src/application/api/addresses/routes.py
+++ src/application/api/addresses/routes.py
@@ -1,9 +1,42 @@
 from flask import jsonify, request
 
+from application.mappers import AddressDTO
 from application.models import Address
+from application.schemas import AddressSchema
+from application.queries.orm.address import AddressORM
 from . import bp
 
 
-@bp.get('/address_get')
-def address_get():
-    return jsonify({'address': 'works'})
+@bp.delete('/delete_address')
+def delete_address():
+    if not request.is_json:
+        return jsonify({'message': 'Request body must be JSON'}), 400
+    
+    try:
+        address_id = int(request.json['address_id'])
+        AddressORM.delete_address(address_id)
+    except Exception as e:
+        return jsonify({'message': f'{e}'})        
+
+    return jsonify({'message': 'Success'}), 200
+
+
+@bp.post('/add_address')
+def add_address():
+    if not request.is_json:
+        return jsonify({'message': 'Request body must be JSON'}), 400
+    
+    try:
+        request_data = request.json
+        address_schema = AddressSchema(**request_data)
+    except Exception as e:
+        return jsonify({'message': f'{e}'}), 400
+    else:
+        new_address = Address(
+            country=address_schema.country,
+            city=address_schema.city,
+            street=address_schema.street
+        )
+        AddressORM.add_address(new_address)
+
+    return jsonify({'message': 'Success'}), 200
--- src/application/queries/orm/address.py
+++ src/application/queries/orm/address.py
@@ -1,11 +1,20 @@
+from application.database import get_db_session
 from application.models import Address
 
 
 class AddressORM:
     @staticmethod
-    def add_address():
-        pass
+    def add_address(address: Address):
+        with get_db_session() as db_session:
+            db_session.add(address)
+            db_session.commit()
 
     @staticmethod
-    def delete_address(id: int):
-        pass
+    def delete_address(address_id: int):
+        with get_db_session() as db_session:
+            address_to_delete = db_session.query(Address).filter(Address.id == address_id).one_or_none()
+            if address_to_delete:
+                db_session.delete(address_to_delete)
+                db_session.commit()
+            else:
+                raise Exception(f'Address with id {address_id} not found')
