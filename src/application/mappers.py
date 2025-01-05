040ec38ab74ef94d119fe34d74dbd724a1c1c433
Almaz Saidov <almaz.saidov.03@bk.ru>
2025-01-05 21:10:05 +0300

    remove Image mapper

--- src/application/mappers.py
+++ src/application/mappers.py
@@ -1,6 +1,6 @@
 import json
 
-from application.models import Address, Client, Image, Product, Supplier
+from application.models import Address, Client, Product, Supplier
 from application.queries.orm.address import AddressORM
 
 
@@ -26,15 +26,6 @@ class ClientDTO:
         self.address = AddressDTO(AddressORM.get_address(client.address_id)).map_address_dto_to_json()
 
     def map_client_dto_to_json(self):
-        return json.dumps(self.__dict__, indent=4)
-
-
-class ImageDTO:
-    def __init__(self, image: Image):
-        self.id = image.id
-        self.data = image.data
-
-    def map_image_dto_to_json(self):
         return json.dumps(self.__dict__, indent=4)
 
 
