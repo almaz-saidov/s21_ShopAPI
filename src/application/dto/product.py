a95e374d43779fdf445c242be111de4603d25583
Almaz Saidov <almaz.saidov.03@bk.ru>
2025-01-12 20:08:56 +0300

    add image and supplier IDs to product DTO

--- src/application/dto/product.py
+++ src/application/dto/product.py
@@ -11,6 +11,8 @@ class ProductDTO:
         self.price = product.price
         self.available_stock = product.available_stock
         self.last_update_date = product.last_update_date.isoformat()
+        self.supplier_id = product.supplier_id
+        self.image_id = product.image_id
 
     def map_product_dto_to_json(self):
         return json.dumps(self.__dict__)
