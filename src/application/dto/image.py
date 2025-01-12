6797f0e37b99e11d0f0ba8ecd8787e4fec87ba30
Almaz Saidov <almaz.saidov.03@bk.ru>
2025-01-12 17:35:27 +0300

    add data field to image DTO

--- src/application/dto/image.py
+++ src/application/dto/image.py
@@ -6,6 +6,7 @@ from application.models.image import Image
 class ImageDTO:
     def __init__(self, image: Image):
         self.id = image.id
+        self.data = image.data
 
     def map_image_dto_to_json(self):
-        return json.dumps(self.__dict__)
+        return json.dumps(self.id)
