bdfa8e3b8082db274af7d1cfe47d7c6edad460b0
Almaz Saidov <almaz.saidov.03@bk.ru>
2025-10-28 16:29:20 +0300

    Убрать лишний роутер

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, FastAPI
+from fastapi import FastAPI
 
 from apps.address.router import router as address_router
 from apps.category.router import router as category_router
@@ -7,15 +7,11 @@ from apps.product.router import router as product_router
 from apps.product_image.router import router as product_image_router
 from apps.supplier.router import router as supplier_router
 
-app = FastAPI()
+app = FastAPI(root_path="/api/v1")
 
-api_router = APIRouter(prefix="/api/v1")
-
-api_router.include_router(address_router)
-api_router.include_router(category_router)
-api_router.include_router(client_router)
-api_router.include_router(product_router)
-api_router.include_router(product_image_router)
-api_router.include_router(supplier_router)
-
-app.include_router(api_router)
+app.include_router(address_router)
+app.include_router(category_router)
+app.include_router(client_router)
+app.include_router(product_router)
+app.include_router(product_image_router)
+app.include_router(supplier_router)
